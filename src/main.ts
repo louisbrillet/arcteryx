fda8a3b25ef0882469a27e773e90bfe5d3c907b0
Thibaud Navarre <navarre.thibaud@gmail.com>
2022-06-21 15:40:56 +0200

    try to access parent document

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,21 @@ WA.onInit()
     bootstrapExtra()
       .then(() => {
         console.log('Scripting API Extra ready');
-        var head = document.getElementsByTagName('head')[0];
-        var link = document.createElement('link');
+        const head = parent.document.getElementsByTagName('head')[0];
+        console.log('head');
+        console.log(head);
+        const htmlParent = head.parentElement;
+        console.log('htmlParent');
+        console.log(htmlParent);
+        const iframeParent = htmlParent && htmlParent.parentElement;
+        console.log('iframeParent');
+        console.log(iframeParent);
+        console.log('windowFrameElement');
+        console.log(window.frameElement);
+        console.log(window.parent);
+        console.log(window.top);
+        console.log(window.document);
+        const link = document.createElement('link');
         link.id = 'customIframeId';
         link.rel = 'stylesheet';
         link.type = 'text/css';
