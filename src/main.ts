a4aac83bcad8f218149af593b38412e835b465d1
Thibaud Navarre <navarre.thibaud@gmail.com>
2022-06-21 13:45:55 +0200

    add clothes and login url

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,17 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
-console.log('Script started successfully');
-
-let currentPopup: any = undefined;
+import { bootstrapExtra } from '@workadventure/scripting-api-extra';
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-
-    WA.room.onEnterLayer('clockZone').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
-    })
-
-    WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
-
+WA.onInit()
+  .then(() => {
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
+    bootstrapExtra()
+      .then(() => {
         console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
-
-}).catch(e => console.error(e));
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+      })
+      .catch((e) => console.error(e));
+  })
+  .catch((e) => console.error(e));
 
 export {};
